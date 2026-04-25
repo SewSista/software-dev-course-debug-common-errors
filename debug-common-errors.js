c003748a1dcc150226ae02f1e8dc151187993901
Clover <sewsistacomo@gmail.com>
2026-04-25 16:20:45 -0500

    made code corrections

--- debug-common-errors.js
+++ debug-common-errors.js
@@ -24,22 +24,22 @@ Think about which debugging methods you found most useful and how you might appl
 // Description:
 // This program is intended to display a simple prompt in the console but fails to run.
 
-console.log("Welcome to the bootcamp
+console.log("Welcome to the bootcamp!"); //added missing closing quote, parenthesis, and semicolon
 
-// What’s Wrong?
+// What’s Wrong? syntax error: missing closing quote with parenthesis and ending ; //
 
 
 // Program B
 // Description:
 // This code attempts to multiply each number in an array by 2 and display the results. However, it crashes at runtime.
 
-let numbers = [2, 4, "eight"];
+let numbers = [2, 4, 8]; //changed string to number for 8
 for (let i = 0; i < numbers.length; i++) {
   let doubled = numbers[i] * 2;
   console.log(doubled);
 }
 
-// What’s Wrong?
+// What’s Wrong? runtime error: numbers included a string instead of number for "8" which cannot be used in an equation
 
 
 
@@ -49,7 +49,7 @@ for (let i = 0; i < numbers.length; i++) {
 
 function isPrime(num) {
   if (num < 2) return false;
-  for (let i = 2; i < num; i++) {
+  for (let i = 1; i < num; i++) { // changed i = 0 to i = 1 to avoid division by zero
     if (num % i === 0) {
       return true;  // Supposed to indicate num is NOT prime
     }
@@ -59,4 +59,4 @@ function isPrime(num) {
 
 console.log(isPrime(7)); // Expected true but gets false
 
-// What’s Wrong?
+// What’s Wrong? logic error: (let i = 2; i < num; i++)
